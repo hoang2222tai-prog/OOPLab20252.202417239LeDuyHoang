b70b79757ca1e564133c56ff4d324be3eb6e5212
hoang2222tai-prog <hoang2222tai@gmail.com>
2026-04-14 16:21:26 +0700

    Update Aims.java

--- src/Aims.java
+++ src/Aims.java
@@ -1,9 +1,9 @@
 public class Aims{
     public static void main(String[] args){
         Cart cart=new Cart();
-        DigitalVideoDisc d1=new DigitalVideoDisc("lion king","hoat hinh","disney",90,19.9f);
-        DigitalVideoDisc d2=new DigitalVideoDisc("star wars","vien tuong","lucas",120,24.5f);
-        DigitalVideoDisc d3=new DigitalVideoDisc("aladin","hoat hinh",18.9f);
+        DigitalVideoDisc d1=new DigitalVideoDisc("lmao koko","hoat hinh","iuyube",90,19.9f);
+        DigitalVideoDisc d2=new DigitalVideoDisc("cafe cafe","vien tuong","lucas",120,24.5f);
+        DigitalVideoDisc d3=new DigitalVideoDisc("amayoo","hoat hinh",18.9f);
         cart.add(d1);
         cart.add(d2);
         cart.add(d3);
